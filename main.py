bdd9d06bae04cfb03475c612fbb7f11d148052e1
Tinusha Priyamanka <153253769+tinusha-priyamanka-99@users.noreply.github.com>
2024-10-12 20:29:02 +0530

    exception,logger and utils added

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.mlproject import logger
+
+logger.info("Welcome to our custom project logging")
--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "mlproject"
 
@@ -25,6 +25,7 @@ list_of_files = [
     "app.py",
     "Dockerfile",
     "requirements.txt",
+    "main.py",
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
